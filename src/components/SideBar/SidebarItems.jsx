fc95cc22ddfdf3dc59a2199b963a5dc307f6528a
lovvejeet <135307361+lovvejeet@users.noreply.github.com>
2024-06-04 13:42:33 +0530

    added dark mode

--- src/components/SideBar/SidebarItems.jsx
+++ src/components/SideBar/SidebarItems.jsx
@@ -1,17 +1,23 @@
+import { Button, useColorMode } from "@chakra-ui/react";
 import CreatePost from "./CreatePosts";
 import Home from "./Home";
-import Notifications from "./Notifications";
+// import Notifications from "./Notifications";
 import ProfileLink from "./ProfileLink";
 import Search from "./Search";
 
 const SidebarItems = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <>
       <Home />
       <Search />
-      <Notifications />
+      {/* <Notifications /> */}
       <CreatePost />
       <ProfileLink />
+      <Button onClick={toggleColorMode}>
+      {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
     </>
   );
 };
